2f23f98d3edcdd539519c85a3f7534f8142b7b5c
dreif8270@gmail.com <dreif8270@gmail.com>
2020-08-18 19:43:40 +0300

    Add Books API

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module ylubyanoy/go_web_server
 
 go 1.15
+
+require github.com/gorilla/mux v1.7.4 // indirect
--- main.go
+++ main.go
@@ -6,12 +6,28 @@ import (
 	"fmt"
 	"log"
 	"math"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"strconv"
 	"text/template"
 	"time"
+
+	"github.com/gorilla/mux"
 )
+
+type Book struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Author *Author `json:"author"`
+}
+
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var books []Book
 
 var apiKey *string // 9ad6a77f98b24af492a46b7e450cc379
 
@@ -154,7 +170,76 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tpl.Execute(w, nil)
 }
 
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range books {
+		fmt.Println(item.ID)
+		fmt.Println(params)
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
+}
+
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(1000000))
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
+}
+
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"]
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+}
+
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+}
+
 func main() {
+	books = append(books, Book{ID: "1", Title: "Война и Мир", Author: &Author{Firstname: "Лев", Lastname: "Толстой"}})
+	books = append(books, Book{ID: "2", Title: "Преступление и наказание", Author: &Author{Firstname: "Фёдор", Lastname: "Достоевский"}})
+
+	r := mux.NewRouter()
+	r.HandleFunc("/books/", getBooks).Methods("GET")
+	r.HandleFunc("/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/books/", createBook).Methods("POST")
+	r.HandleFunc("/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/books/{id}", deleteBook).Methods("DELETE")
+
 	apiKey = flag.String("apikey", "", "Newsapi.org access key")
 	flag.Parse()
 	if *apiKey == "" {
@@ -162,14 +247,16 @@ func main() {
 	}
 
 	port := "8000"
-	mux := http.NewServeMux()
+	smux := http.NewServeMux()
 
 	fs := http.FileServer(http.Dir("assets"))
-	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+	smux.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
-	mux.HandleFunc("/search", searchHandler)
-	mux.HandleFunc("/", indexHandler)
-	err := http.ListenAndServe(":"+port, mux)
+	smux.Handle("/books/", r)
+	smux.HandleFunc("/search", searchHandler)
+	smux.HandleFunc("/", indexHandler)
+
+	err := http.ListenAndServe(":"+port, smux)
 	if err != nil {
 		fmt.Println(err)
 
